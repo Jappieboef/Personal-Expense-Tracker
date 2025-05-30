21d73ff22eb3d86bcfc6aff7589aa8d4a658150b
Jasper Steenwinkel <Jasperevertsteenwinkel@gmail.com>
2025-05-30 15:08:36 -0500

    app.py

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -1,5 +1,12 @@
-
 import pandas as pd
+
+
+
 
 df = pd.read_csv('synthetic_expense_data.csv')
 print(df.head())
+print(df.describe())
+# Display the first few rows of the DataFrame
+print(df.columns)
+food_expenses = df[df['Category'] == 'Groceries']
+print(food_expenses['Amount'].sum())
