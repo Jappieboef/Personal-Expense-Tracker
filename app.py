03b6bb95c0d8c3a5bb47af309b4e3268ce563fcf
Jasper Steenwinkel <Jasperevertsteenwinkel@gmail.com>
2025-05-30 16:02:55 -0500

    added import is

--- app.py
+++ app.py
@@ -27,4 +27,8 @@ def view_expenses():
         return "No expenses recorded yet!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
